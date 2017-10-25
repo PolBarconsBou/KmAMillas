7db0acc088bd6514644e3a2e9c6a23df756ec833
PolBarconsBou <polbarcons@gmail.com>
2017-10-25 18:14:26 +0200

    KmAMillas acabat

--- app/src/main/java/edu/upc/eseiaat/pma/kmamillas/MainActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/kmamillas/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.btn_a_millas);
+        Button btn_a_millas = (Button) findViewById(R.id.btn_a_millas);
+        Button btn_a_km = (Button) findViewById(R.id.btn_a_km);
+
         final EditText edit_km = (EditText) findViewById(R.id.edit_km);
         final EditText edit_millas = (EditText) findViewById(R.id.edit_millas);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_a_millas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String skm = edit_km.getText().toString();
@@ -25,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
                 float millas = km / 1.609f;
                 String smillas = String.format("%f", millas);
                 edit_millas.setText(smillas);
+
+
+            }
+        });
+
+        btn_a_km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String smillas = edit_millas.getText().toString();
+                float millas = Float.parseFloat(smillas);
+                float km = millas * 1.609f;
+                String skm = String.format("%f", km);
+                edit_km.setText(skm);
             }
         });
     }
