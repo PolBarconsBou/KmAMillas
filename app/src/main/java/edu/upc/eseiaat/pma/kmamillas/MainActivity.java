4540a9581cd364014668e3de6937677ae09ca58c
PolBarconsBou <polbarcons@gmail.com>
2017-10-25 17:57:33 +0200

    Ja funciona la conversió de Km a Milles (falta al revés)

--- app/src/main/java/edu/upc/eseiaat/pma/kmamillas/MainActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/kmamillas/MainActivity.java
@@ -2,6 +2,9 @@ package edu.upc.eseiaat.pma.kmamillas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn_a_millas);
+        final EditText edit_km = (EditText) findViewById(R.id.edit_km);
+        final EditText edit_millas = (EditText) findViewById(R.id.edit_millas);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skm = edit_km.getText().toString();
+                float km = Float.parseFloat(skm);
+                float millas = km / 1.609f;
+                String smillas = String.format("%f", millas);
+                edit_millas.setText(smillas);
+            }
+        });
     }
 }
